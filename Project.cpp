84e9c66cf4aba2cd1f12f194d3c249782837e3df
shiavngi-jain-08 <myselfshivangi08@gmail.com>
2024-09-28 14:27:16 +0530

    add TrafficSignal class back to the code

--- Project.cpp
+++ Project.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-class Vehicle {
+class Vehicle
+{
 private:
     string type;
     int speed;
@@ -12,35 +13,42 @@ public:
     static int vehicleCount;
     static int totalSpeed;
 
-    Vehicle(string type, int speed) {
+    Vehicle(string type, int speed)
+    {
         this->type = type;
         this->speed = speed;
         vehicleCount++;
         totalSpeed += speed;
     }
 
-    ~Vehicle() {
+    ~Vehicle()
+    {
         vehicleCount--;
         totalSpeed -= speed;
     }
 
-    void displayInfo() {
+    void displayInfo()
+    {
         cout << "Vehicle Type: " << this->type << "\nSpeed: " << this->speed << " km/h" << endl;
     }
 
-    static void displayVehicleCount() {
+    static void displayVehicleCount()
+    {
         cout << "Total Vehicles: " << vehicleCount << endl;
     }
 
-    static void displayTotalSpeed() {
+    static void displayTotalSpeed()
+    {
         cout << "Total Speed of All Vehicles: " << totalSpeed << " km/h" << endl;
     }
 
-    static double averageSpeed() {
+    static double averageSpeed()
+    {
         return vehicleCount > 0 ? static_cast<double>(totalSpeed) / vehicleCount : 0;
     }
 
-    void move() {
+    void move()
+    {
         cout << this->type << " is moving at " << this->speed << " km/h." << endl;
     }
 };
@@ -48,13 +56,40 @@ public:
 int Vehicle::vehicleCount = 0;
 int Vehicle::totalSpeed = 0;
 
-int main() {
-    Vehicle* vehicles[3];
+class TrafficSignal
+{
+private:
+    string color;
+    int timer;
+
+public:
+    TrafficSignal(string color, int timer)
+    {
+        this->color = color;
+        this->timer = timer;
+    }
+
+    void changeSignal(string newColor)
+    {
+        this->color = newColor;
+        cout << "Traffic signal changed to " << this->color << endl;
+    }
+
+    void displayStatus()
+    {
+        cout << "Traffic Signal: " << this->color << "\nTimer: " << this->timer << " seconds" << endl;
+    }
+};
+
+int main()
+{
+    Vehicle *vehicles[3];
     vehicles[0] = new Vehicle("Car", 80);
     vehicles[1] = new Vehicle("Bus", 50);
     vehicles[2] = new Vehicle("Bike", 60);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         vehicles[i]->displayInfo();
         vehicles[i]->move();
     }
@@ -63,7 +98,18 @@ int main() {
     Vehicle::displayTotalSpeed();
     cout << "Average Speed of All Vehicles: " << Vehicle::averageSpeed() << " km/h" << endl;
 
-    for (int i = 0; i < 3; i++) {
+    TrafficSignal signals[2] = {
+        TrafficSignal("Red", 30),
+        TrafficSignal("Green", 45)};
+
+    for (int i = 0; i < 2; i++)
+    {
+        signals[i].displayStatus();
+        signals[i].changeSignal(i == 0 ? "Green" : "Red");
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
         delete vehicles[i];
     }
 
